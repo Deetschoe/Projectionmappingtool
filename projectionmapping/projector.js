f223f127da026197c4b9ed3172b503582623c1e1
deetschoe <deetschoening@gmail.com>
2025-12-30 14:44:36 -0500

    Remove duplicate files - files moved to css/ and js/ folders

--- projectionmapping/projector.js
+++
@@ -1,7 +0,0 @@
-// Projector window communication helper
-// This file is included in main.js but kept separate for organization
-
-// The projector window logic is in projector.html
-// This file can be used for additional projector-specific functionality if needed
-
-
